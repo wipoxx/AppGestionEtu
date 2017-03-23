ca1f0632c5d0083000b684f9ad2c525d3b443823
wipoxx <marine.camuset@hotmail.com>
2017-03-23 10:13:01 -0400

    Ajout du début du projet Android Studio + README.md

---
+++ GestionVieEtu/app/src/main/java/uqac/gestionvieetu/MainFragment.java
@@ -0,0 +1,17 @@
+package uqac.gestionvieetu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//Le fragment qui contient le layout de la page d'accueil
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_main, container, false);
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# AppGestionEtu
+
+Pour ajouter son layout :
+* Créer le layout
+* Créer une classe java qui étend Fragment et qui inflate le layout (cf EtudesFragment)
+* Dans le fichier content_main.xml, ajouter un attribut onClick au bouton correspondant
+* Dans le fichier MainActivity.java, remplacer le fragment actuellement affiché par le nouveau fragment (cf la méthode afficherLayoutEtudes)
+
+-> Il n’y a qu’une activité, la seule chose qui change est le contenu du fragment
+
